1de7be3c0bd7a59466d92a195b3a3e31dea28d23
Yash Garg <yg292001@gmail.com>
2025-11-20 16:13:28 +0530

    improved portfolio with animations and changes layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
 "use client";
-
+import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
-import { ExternalLink, Github, Linkedin, Moon, Sun, Twitter } from "lucide-react";
+import { FileText, Github, Linkedin, Mail, Moon, Sun, Twitter } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
+import ProjectsSection from "@/components/custom/projects";
+
+const narutoQuotes = [
+  "I never go back on my word—that’s my ninja way!",
+  "I’ll become Hokage, no matter what!",
+  "I won’t let my friends get hurt.",
+  "Believe it!",
+  "I carry everyone’s hopes on my back.",
+  "I’m not alone anymore.",
+  "You’re not taking my bond away."
+];
+
+const sasukeQuotes = [
+  "I walk my own path.",
+  "My goal is revenge. Nothing more.",
+  "I won’t let my past define me.",
+  "You’re still too weak.",
+  "I sever bonds… to grow stronger.",
+  "Hatred is my fuel.",
+  "I have my own version of justice."
+];
+
 export default function Home() {
   const skills = [
     'React',
@@ -28,10 +50,16 @@ export default function Home() {
 
   return (
     <main className="min-h-screen flex flex-col items-center justify-center px-4 py-12 bg-[var(--background)] text-[var(--foreground)]">
-      <div className="w-full md:w-7/10 flex justify-end items-center gap-2 mb-8">
+      <motion.div initial={{ x: 50, opacity: 0 }}      // start off-screen right
+        animate={{ x: 0, opacity: 1 }}       // move to center
+        transition={{
+          duration: 0.5,
+          ease: "easeOut"
+        }}
+        className="w-full md:w-7/10 flex justify-end items-center gap-2 mb-8">
         {resolvedTheme === "dark" ?
-          "I walk the path of shadows 🌒"
-          : "I shine with the power of the sun ☀️"}
+          sasukeQuotes[Math.floor(Math.random() * sasukeQuotes.length)]
+          : narutoQuotes[Math.floor(Math.random() * narutoQuotes.length)]}
 
         <Button
           variant="outline"
@@ -45,49 +73,98 @@ export default function Home() {
           )}
           <span className="sr-only">Toggle theme</span>
         </Button>
-      </div>
+      </motion.div>
       {/* ABOUT SECTION */}
-      <section className="w-full max-w-2xl text-center mb-16">
+      <motion.section
+        initial={{ y: 40, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeOut" }} className="w-full max-w-2xl text-center mb-16" >
         <Image src={"/profileAvatar.png"} alt="Yash Garg" width={100} height={100} className="mx-auto my-3" />
         <h1 className="text-4xl md:text-5xl font-bold mb-2 tracking-tight">Yash Garg</h1>
         <h2 className="text-xl md:text-2xl font-mono text-[var(--color-blue-400)] mb-4">Full Stack Developer</h2>
-        <p className="text-[var(--foreground)] my-2">Email : <Link href="mailto:yg292001@gmail.com" prefetch={false} className="underline">yg292001@gmail.com</Link>{" :)"}</p>
+        <p className="flex gap-2 justify-center my-2">
+          <Button variant={"outline"} className="hover:scale-103 transition-transform">
+            <Link href="https://drive.google.com/file/d/1wMFjoYZUTdg0rs17RxET6tVH8vP9-9Xs/view?usp=sharing" className="flex gap-1 items-center">
+              Resume <FileText />
+            </Link>
+          </Button>
+          <Button variant={"outline"} className="hover:scale-103 transition-transform ">
+            <Link href="mailto:yg292001@gmail.com" className="flex gap-1 items-center">
+              Mail <Mail />
+            </Link>
+          </Button>
+        </p>
+        {/* <p className="text-[var(--foreground)] my-2">Email : <Link href="mailto:yg292001@gmail.com" prefetch={false} className="underline">yg292001@gmail.com</Link>{" :)"}</p> */}
 
         <p className="text-base md:text-lg text-[var(--foreground)] max-w-xl mx-auto whitespace-pre-wrap">
-          I am a programmer who can fix annoying bugs, create something awesome, or make someone&apos;s dream a reality.</p>
+          A programmer who can fix annoying bugs, create something awesome, or make someone&apos;s dream a reality.</p>
 
-      </section>
+      </motion.section>
 
       {/* TECH STACK SECTION */}
-      <section className="w-full max-w-2xl mb-16">
+      <motion.section initial={{ y: 40, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeOut" }} className="w-full max-w-2xl mb-16">
         <h3 className="text-lg font-semibold mb-4 text-left">Tech Stack</h3>
-        <ul className="flex flex-wrap gap-4 justify-center md:justify-start text-sm text-gray-300">
-          {skills.map((skill, index) => <li key={index} className="px-3 py-1 border border-[var(--border)] text-[var(--foreground)] rounded-full hover:opacity-80">{skill}</li>)}
-        </ul>
-      </section>
+        <div className="overflow-hidden whitespace-nowrap relative w-full">
+          <motion.div
+            className="flex gap-4"
+            initial={{ x: "0%" }}
+            animate={{ x: "-50%" }}
+            transition={{
+              duration: 70,      // speed of scroll
+              ease: "linear",
+              repeat: Infinity,  // infinite loop
+              repeatType: "loop"
+            }}
+            style={{ width: "max-content" }}
+          >
+            {/* Duplicate list to make it loop seamlessly */}
+            {[...skills, ...skills].map((skill, idx) => (
+              <span
+                key={idx}
+                className="px-3 py-1 border border-[var(--border)] text-[var(--foreground)] rounded-full hover:opacity-80"
+              >
+                {skill}
+              </span>
+            ))}
+          </motion.div>
+        </div>
+      </motion.section>
 
       {/* PROJECTS SECTION */}
-      <section className="w-full max-w-2xl mb-8">
+
+      <ProjectsSection isHomePage={true} />
+      {/* <section className="w-full max-w-2xl mb-8">
         <h3 className="text-base font-semibold mb-4 text-left flex gap-2">My Latest Resume - <Link className="flex gap-0.5 underline items-center" href={"https://drive.google.com/file/d/1wMFjoYZUTdg0rs17RxET6tVH8vP9-9Xs/view?usp=sharing"}>Google Drive <ExternalLink size={16} /></Link></h3>
         <h3 className="text-base font-semibold mb-4 text-left">Projects I have built - <Link className="underline" href={"/projects"}>/projects</Link></h3>
         <h3 className="text-base font-semibold mb-4 text-left">My Holopin badges - <Link className="underline" href={"/holopin"}>/holopin</Link></h3>
         <h3 className="text-base font-semibold mb-4 text-left">Check out my website - <Link className="underline" href={"https://www.driplist.in"}>DripList.in</Link> and my <Link className="underline" href={"https://www.driplist.in/u/mrmods"}>profile</Link> on it.</h3>
-      </section>
-      {/* <ProjectsSection isHomePage={true} /> */}
-      {/* BADGES SECTION */}
-      {/* <section className="w-full max-w-2xl mb-16">
-        <h3 className="text-lg font-semibold mb-2">Badges</h3>
-        <Image src={'/holopin.png'} width={"600"} height={"200"} alt="holpoin board"></Image>
       </section> */}
 
+
+
+      {/* BADGES SECTION */}
+      <motion.section initial={{ y: 40, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeOut" }} className="w-full max-w-2xl mb-16">
+        <h3 className="text-lg font-semibold mb-2">Badges</h3>
+        <Image src={'/holopin.png'} width={"600"} height={"200"} alt="holpoin board"></Image>
+      </motion.section>
+
       {/* CONTACT SECTION */}
-      <section className="w-full max-w-2xl border-t border-[#232323] pt-8">
+      <motion.section initial={{ y: 40, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeOut" }} className="w-full max-w-2xl border-t border-[#232323] pt-8">
         <div className="flex justify-start gap-4">
           <a href="https://github.com/mr-mods-yg" aria-label="GitHub" className="p-2 border rounded-md hover:bg-blue-800 hover:text-white"><Github />GitHub</a>
           <a href="https://www.linkedin.com/in/mr-mods/" aria-label="LinkedIn" className="p-2 border rounded-md hover:bg-blue-800 hover:text-white"><Linkedin />LinkedIn</a>
           <a href="https://x.com/mr_mods_yg" aria-label="X (Twitter)" className="p-2 border rounded-md hover:bg-blue-800 hover:text-white"><Twitter /> (Twitter)</a>
         </div>
-      </section>
+        <div className="pt-10 text-lg">
+          Thank you for visiting {"(๑ > ᴗ < ๑)"}
+        </div>
+      </motion.section>
     </main>
   );
 }
--- src/components/custom/projects.tsx
+++ src/components/custom/projects.tsx
@@ -1,107 +1,127 @@
+"use client";
 import Image from "next/image"
 import Link from "next/link"
-
+import { motion } from "framer-motion";
 function ProjectsSection({ isHomePage, isSideProjects }: { isHomePage: boolean, isSideProjects?: boolean }) {
     if (isSideProjects) {
-        return <section className="w-full max-w-2xl mb-16">
+        return <motion.section initial={{ y: 40, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeOut" }} className="w-full max-w-2xl mb-16">
+
             <div className="grid gap-6 md:grid-cols-2">
-                <div className="bg-(var(--background)) rounded-lg p-5 text-left border border-(var(--border)) hover:scale-105 transition-transform">
-                    <div className="mb-4" style={{ position: 'relative', width: '100%', height: '160px' }}>
-                        <Image
-                            src="/torrent-search.png"
-                            alt="Torrent Search Image"
-                            fill
-                            style={{ objectFit: 'fill' }}
-                        />
-                    </div>
-                    <h4 className="font-bold mb-1">Torrent Search</h4>
-                    <p className="text-gray-400 text-sm mb-2">A simple torrent search client built using public APIs, Flask (Python).</p>
-                    <a href="https://torrent-search-pi.vercel.app/" className="mr-3 underline text-[var(--accent)] text-md">Demo</a>
-                    <Link href="https://github.com/mr-mods-yg/torrent-search" className="underline mr-3 text-[var(--accent)] text-md">Github</Link>
-                </div>
-                <div className="bg-(var(--background)) rounded-lg p-5 text-left border border-(var(--border)) hover:scale-105 transition-transform">
-                    <div className="mb-4" style={{ position: 'relative', width: '100%', height: '160px' }}>
-                        <Image
-                            src="/discord-moderation-bot.png"
-                            alt="Discord Moderation bot Image"
-                            fill
-                            style={{ objectFit: 'fill' }}
-                        />
-                    </div>
-                    <h4 className="font-bold mb-1">Discord Moderation Bot</h4>
-                    <p className="text-gray-400 text-sm mb-2">A discord bot that helps server owners and administrators manage their online communities on Discord built using Python.</p>
-                    <Link href="https://github.com/mr-mods-yg/moderation-fun-bot-python" className="underline mr-3 text-[var(--accent)] text-md">Github</Link>
-                    {/* <a href="https://github.com/mr-mods-yg/driplist-next" className="mr-3 underline text-[var(--accent)] text-md">Github</a> */}
-                </div>
+                <ProjectCard
+                    imagePath="/torrent-search.png"
+                    imageAlt="Torrent Search Image"
+                    name="Torrent Search"
+                    description="A simple torrent search client built using Flask (Python) that provides the service using public APIs."
+                    isWorkInProgress={true}
+                    technologlies={["Python", "Flask", "REST API"]}
+                    liveURL="https://torrent-search-pi.vercel.app/"
+                    githubURL="https://github.com/mr-mods-yg/torrent-search"
+                />
+                <ProjectCard
+                    imagePath="/discord-moderation-bot.png"
+                    imageAlt="Discord Moderation bot Image"
+                    name="Discord Moderation Bot"
+                    description="A discord bot that helps server owners and administrators manage their online communities on Discord."
+                    isWorkInProgress={true}
+                    technologlies={["Python", "discord.py", "REST API"]}
+                    githubURL="https://github.com/mr-mods-yg/moderation-fun-bot-python"
+                />
+
             </div>
-        </section>
+        </motion.section>
     }
-    return <section className="w-full max-w-2xl mb-16">
+    return <motion.section initial={{ y: 40, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeOut" }} className="w-full max-w-2xl mb-16">
+        {isHomePage && <h3 className="text-lg font-semibold mb-2 text-start">Projects</h3>}
         <div className="grid gap-6 md:grid-cols-2">
-            <div className="bg-(var(--background)) rounded-lg p-5 text-left border border-(var(--border)) hover:scale-105 transition-transform">
-                <div className="mb-4" style={{ position: 'relative', width: '100%', height: '160px' }}>
-                    <Image
-                        src="/driplist.png"
-                        alt="DripList Image"
-                        fill
-                        style={{ objectFit: 'cover' }}
-                    />
-                </div>
-                <h4 className="font-bold mb-1">DripList <span className="inline-flex items-center mx-2 rounded-md bg-muted px-2.5 py-0.5 text-sm font-medium text-green-500 ring-1 ring-inset ring-green-500">
-                    Work in Progress
-                </span></h4>
-                <p className="text-gray-400 text-sm mb-2">A platform for tracking your watched content, sharing reviews and discovering content built using NextJS.</p>
-                <a href="https://driplist.in/" className="underline mr-3 text-[var(--accent)] text-md">DripList.in</a>
-                {/* <a href="https://github.com/mr-mods-yg/driplist-next" className="mr-3 underline text-[var(--accent)] text-md">Github</a> */}
-            </div>
-            <div className="bg-(var(-background)) rounded-lg p-5 text-left border border-(var(--border)) hover:scale-105 transition-transform">
-                <div className="mb-4" style={{ position: 'relative', width: '100%', height: '160px' }}>
-                    <Image
-                        src="/virtualmeet.png"
-                        alt="VirtualMeet Image"
-                        fill
-                        style={{ objectFit: 'cover' }}
-                    />
-                </div>
-                <h4 className="font-bold mb-1">VirtualMeet</h4>
-                <p className="text-gray-400 text-sm mb-2">A virtual event management platform where you can create, manage and explore events built using MERN stack</p>
-                <a href="https://virtual-meet-psi.vercel.app/" className="underline mr-3 text-[var(--accent)] text-md">Demo</a>
-                <a href="https://github.com/mr-mods-yg/VirtualMeet" className="mr-3 underline text-[var(--accent)] text-md">Github</a>
-            </div>
-            <div className="bg-(var(-background)) rounded-lg p-5 text-left border border-(var(--border)) hover:scale-105 transition-transform">
-                <div className="mb-4" style={{ position: 'relative', width: '100%', height: '160px' }}>
-                    <Image
-                        src="/shadowtalk.png"
-                        alt="ShadowTalk Image"
-                        fill
-                        style={{ objectFit: 'cover' }}
-                    />
-                </div>
-                <h4 className="font-bold mb-1">ShadowTalk</h4>
-                <p className="text-gray-400 text-sm mb-2">A real-time room based chat platform with additional functionality of sending images built with MERN stack</p>
-                <a href="https://shadow-talk-chat-app.vercel.app/" className="mr-3 underline text-[var(--accent)] text-md">Demo</a>
-                <a href="https://github.com/mr-mods-yg/shadow-talk-chat-app" className="mr-3 underline text-[var(--accent)] text-md">Github</a>
-            </div>
-            <div className="bg-(var(-background)) rounded-lg p-5 text-left border border-(var(--border)) hover:scale-105 transition-transform">
-                <div className="mb-4" style={{ position: 'relative', width: '100%', height: '160px' }}>
-                    <Image
-                        src="/bytesEditor.png"
-                        alt="BytesEditor Image"
-                        fill
-                        style={{ objectFit: 'cover' }}
-                    />
-                </div>
-                <h4 className="font-bold mb-1">Bytes code editor</h4>
-                <p className="text-gray-400 text-sm mb-2">A simple code editor for creating java programs built with Java, Swing(AWT).</p>
-                <a href="https://github.com/mr-mods-yg/Bytes-code-editor" className="mr-3 underline text-[var(--accent)] text-md">Github (with images)</a>
-            </div>
+            <ProjectCard
+                imagePath="/driplist.png"
+                imageAlt="DripList Image"
+                name="DripList"
+                description="A platform for tracking your watched content, sharing reviews and discovering content."
+                isWorkInProgress={true}
+                technologlies={["NextJS", "TypeScript", "TailwindCSS", "shadcn/ui", "Prisma", "PostgreSQL"]}
+                liveURL="https://driplist.in/"
+            />
+
+            <ProjectCard
+                imagePath="/virtualmeet.png"
+                imageAlt="VirtualMeet Image"
+                name="VirtualMeet"
+                description="A virtual event management platform where you can create, manage and explore events."
+                technologlies={["ReactJS", "MongoDB", "TailwindCSS", "ExpressJS", "Mongoose ORM"]}
+                liveURL="https://virtual-meet-psi.vercel.app/"
+                githubURL="https://github.com/mr-mods-yg/VirtualMeet"
+            />
+
+            <ProjectCard
+                imagePath="/shadowtalk.png"
+                imageAlt="ShadowTalk Image"
+                name="ShadowTalk"
+                description="A real-time room based chat platform with additional functionality of sending images."
+                technologlies={["ReactJS", "TailwindCSS", "ExpressJS", "Socket.io", "DaisyUI"]}
+                liveURL="https://shadow-talk-chat-app.vercel.app/"
+                githubURL="https://github.com/mr-mods-yg/shadow-talk-chat-app"
+            />
+
+            <ProjectCard
+                imagePath="/bytesEditor.png"
+                imageAlt="BytesEditor Image"
+                name="Bytes code editor"
+                description="A simple code editor for creating, modifiying or/and debugging java programs."
+                technologlies={["Java", "Swing(AWT)", "File Handling", "Terminal Management"]}
+                githubURL="https://github.com/mr-mods-yg/Bytes-code-editor"
+            />
             {isHomePage ?
                 <Link href="/projects" className="mt-4 underline block">View All Projects</Link>
                 :
                 <></>
             }
         </div>
-    </section>
+    </motion.section>
 }
 
+function TechnolgiesBadge({ text }: { text: string }) {
+    return <span className="py-1 px-2 outline-1 rounded-sm">{text}</span>
+}
+
+function ProjectCard({ imagePath, imageAlt, name, isWorkInProgress, description, technologlies, liveURL, githubURL }: {
+    imagePath: string,
+    imageAlt: string,
+    name: string,
+    isWorkInProgress?: boolean,
+    description: string,
+    technologlies: string[],
+    liveURL?: string,
+    githubURL?: string,
+}) {
+    return <div className="bg-[var(--card)] text-[var(--card-foreground)] rounded-lg text-left">
+        <div className="mb-4 relative aspect-video">
+            <Image
+                src={imagePath}
+                alt={imageAlt}
+                fill
+                className="h-full w-full rounded-lg object-cover"
+            />
+        </div>
+        <div className="px-5 pb-5">
+            <h4 className="font-bold mb-1">{name}
+                {isWorkInProgress && isWorkInProgress === true && <span className="inline-flex items-center mx-2 rounded-md bg-muted px-2.5 py-0.5 text-sm font-medium text-green-500 ring-1 ring-inset ring-green-500">
+                    Work in Progress
+                </span>}</h4>
+            <p className="text-sm mb-2">{description}</p>
+            <p className="text-sm mb-2 flex gap-1 items-center flex-wrap">
+                {technologlies.map((technologlies) => <TechnolgiesBadge key={technologlies} text={technologlies} />)}
+            </p>
+            <p className="flex gap-1">
+                {liveURL && <a href={liveURL} className="underline mr-3 text-[var(--accent)] text-md flex">Live</a>}
+                {githubURL && <a href={githubURL} className="mr-3 underline text-[var(--accent)] text-md">Github</a>}
+            </p>
+
+        </div>
+    </div>
+}
 export default ProjectsSection
