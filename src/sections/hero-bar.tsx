e3d7c4078cce94fda8d69050f737b920a7f0ed12
Yash Garg <yg292001@gmail.com>
2025-11-26 11:53:24 +0530

    fixed github redirect in hero bar

--- src/sections/hero-bar.tsx
+++ src/sections/hero-bar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { motion } from "framer-motion";
 import { Clock } from "@/components/custom/clock";
 import { Button } from "@/components/ui/button";
-import {  Github, Moon, Sun } from "lucide-react";
+import { Github, Moon, Sun } from "lucide-react";
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
 
@@ -11,33 +11,36 @@ function HeroBar() {
   const { resolvedTheme, setTheme } = useTheme();
   return (
     <motion.section initial={{ x: 50, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{
-          duration: 0.5,
-          ease: "easeOut"
-        }}
-        className="w-full max-w-2xl text-center mb-8 flex justify-between items-center gap-2">
-        <Clock />
-        <span className="flex gap-1">
-          <Button
-            variant="outline"
-            size="icon"
-            className="rounded-xs"
-            onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-          >
-            {resolvedTheme === "dark" ? (
-              <Sun className="h-5 w-5" />
-            ) : (
-              <Moon className="h-5 w-5" />
-            )}
-            <span className="sr-only">Toggle theme</span>
-          </Button>
+      animate={{ x: 0, opacity: 1 }}
+      transition={{
+        duration: 0.5,
+        ease: "easeOut"
+      }}
+      className="w-full max-w-2xl text-center mb-8 flex justify-between items-center gap-2">
+      <Clock />
+      <span className="flex gap-1">
+        <Button
+          variant="outline"
+          size="icon"
+          className="rounded-xs"
+          onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+        >
+          {resolvedTheme === "dark" ? (
+            <Sun className="h-5 w-5" />
+          ) : (
+            <Moon className="h-5 w-5" />
+          )}
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+        <Link href={"https://github.com/mr-mods-yg/mrmods"}>
           <Button variant="outline"
             size="icon"
-            className="rounded-xs"><Link href={"https://github.com/mr-mods-yg/mrmods"}><Github /></Link></Button>
-        </span>
+            className="rounded-xs"><Github />
+          </Button>
+        </Link>
+      </span>
 
-      </motion.section>
+    </motion.section>
   )
 }
 
