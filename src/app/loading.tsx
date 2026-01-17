1c2ab0ba3e04785a89de1c7ab9c50522a1b1db64
Yash Garg <yg292001@gmail.com>
2026-01-17 22:24:55 +0530

    removed accidentally added characters! no offense

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Loading() {
   return (
     <div className='min-h-svh flex justify-center items-center text-2xl'>
-      Almost there..``
+      Almost there..
     </div>
   )
 }
